bfe389a44a371e76800a3d11edc75f93ff918bfb
manoj kumar <manojthakur614@gmail.com>
2025-02-27 23:33:54 +0530

    git

---
+++ backend/config/config.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL),
+      {
+        useNewUrlParser: true, // Parses MongoDB connection strings
+        useUnifiedTopology: true, // Enables the new server discovery and monitoring engine
+      };
+    console.log("DB connected");
+  } catch (error) {
+    console.log("Error in connecting DB", error.message);
+  }
+};
+
+module.exports = connectMongoDB;
+
---
+++ backend/services/admin.service.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+
+class adminService {
+    verifyToken(token) {
+        try {
+            return jwt.verify(token, process.env.SECREATE_KEY);
+        } catch (err) {
+            return null;
+        }
+    }
+}
+
+module.exports = adminService;
